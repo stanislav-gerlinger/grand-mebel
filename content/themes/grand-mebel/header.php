b6dcd36c62cb490076eda48354a009d6a11ec742
Ilias-19 <as.ilyas2001@mail.ru>
2020-08-23 11:01:54 +0400

    23.08

--- content/themes/grand-mebel/header.php
+++ content/themes/grand-mebel/header.php
@@ -8,67 +8,148 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<?php /* RSS и всякое */ ?>
-	<link rel="alternate" type="application/rdf+xml" title="RDF mapping" href="<?php bloginfo('rdf_url'); ?>">
-	<link rel="alternate" type="application/rss+xml" title="RSS" href="<?php bloginfo('rss_url'); ?>">
-	<link rel="alternate" type="application/rss+xml" title="Comments RSS" href="<?php bloginfo('comments_rss2_url'); ?>">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <?php /* RSS и всякое */ ?>
+    <link rel="alternate" type="application/rdf+xml" title="RDF mapping" href="<?php bloginfo('rdf_url'); ?>">
+    <link rel="alternate" type="application/rss+xml" title="RSS" href="<?php bloginfo('rss_url'); ?>">
+    <link rel="alternate" type="application/rss+xml" title="Comments RSS"
+          href="<?php bloginfo('comments_rss2_url'); ?>">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>"/>
 
     <link
-        rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-        crossorigin="anonymous">
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+            integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+            crossorigin="anonymous">
     <script
-        src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
+            src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
     </script>
     <script
-        src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossorigin="anonymous">
+            src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+            crossorigin="anonymous">
 
     </script>
-	<!--[if lt IE 9]>
-	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
-	<?php wp_head(); ?>
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-	<header>
-		<div class="container">
-			<div class="row">
-				<div class="col-sm-6 col-md-4 col-lg-3" style="background-color: #00aadc;">
-					kupibutton
-                    <button class="btn btn-secondary">...</button>
-				</div>
-                <div class="col-sm-6 col-md-4 col-lg-3" style="background-color: #00aadc;">
-					kupibaton
-				</div>
-                <div class="col-sm-6 col-md-4 col-lg-3" style="background-color: #00aadc;">
-					kupibaton
-				</div>
-			</div>
-		</div>
-        <div class="progress">
-            <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-5">
+                <div>
+                    WhatsApp Telegram
+                </div>
+                <div>
+        <span href="tel:+74959241129">
+          +7 495 924 11 29
+        </span>
+                    <span href="tel:+79851932830">
+          +7 985 193 28 30
+        </span>
+                </div>
+            </div>
+
+            <div class="col-sm-2">
+                <a href="https://shaparbrand.ru" class="logo">
+                    <img title="Логотип бренда Shapar" alt="На фотографии логотип бренда вязаной одежды Shapar"
+                         src="https://shaparbrand.ru/wp-content/themes/test1/img/logo1.svg" width="133px"
+                         height="100px">
+                </a>
+            </div>
+
+            <div class="col-sm-3 user-info">
+                <a href="https://shaparbrand.ru/wp-login.php?action=register&redirect_to=/">
+                    Зарегистрироваться
+                </a>
+                <a href="https://shaparbrand.ru/wp-login.php?redirect_to=%2F">
+                    Войти
+                </a>
+            </div>
+
+            <div class="col-sm-2">
+                <div class="header-c">
+                    <a href="/podarochnyj-sertifikat" class="go-to-cert" title="Подарочная карта">
+                        <img src="https://shaparbrand.ru/wp-content/themes/test1/img/icons/Gift.svg"
+                             alt="Подарочная карта" class="go-to-cert__icon">
+                    </a>
+                    <a href="/izbrannoe/" class="gotofavorite" title="Избранное">
+                        <img src="https://shaparbrand.ru/wp-content/themes/test1/img/icons/Like.svg" alt="Избранное"
+                             class="go-to-cert__icon">
+                        <span class="amount">0</span>
+                    </a>
+                    <a href="/profile/cart/" class="gotocart">
+                        <img src="https://shaparbrand.ru/wp-content/themes/test1/img/icons/Bag.svg" alt="Корзина"
+                             class="go-to-cert__icon">
+                        <span class="amount">0</span>
+                    </a>
+                </div>
+            </div>
         </div>
-	</header>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse" id="topnav">
+                <?php $args = array(
+                    'theme_location' => 'top',
+                    'container'=> false,
+                    'menu_id' => 'top-nav-ul',
+                    'items_wrap' => '<ul id="%1$s" class="nav navbar-nav %2$s">%3$s</ul>',
+                    'menu_class' => 'top-menu',
+                    'walker' => new bootstrap_menu(true)
+                );
+                wp_nav_menu($args);
+                ?>
+            </div>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
 
-    <main>
+</header>
+
+<main>
     <script>
-        var i = 0;
-        var pb = $(".progress-bar");
-        var timerID = setInterval(() => {
-            i++;
-            if(i>=100) {
-                clearInterval(timerID);
-            }
-            pb.css({width:i+"%"})
-            console.log(i);
-        }, 100)
+        let Product = function (data) {
+            // attributes
+            this.price = data.price;
+            this.title = data.title;
+            this.id = data.id;
+            this.inCart = data.inCart;
 
+            // methods
+            this.getInfo = function () {
+                return this.price + this.title + this.id;
+            }
+            this.addToCart = function () {
+                if(this.inCart == false) {
+                    cart.push(this);
+                    this.inCart = true;
+                } else {
+                    alert("Вы уже добавили этот товар в корзину! А теперь иди купи button в Эльдорадо");
+                }
+
+            }
+        };
+        let products = [];
+        for (let i = 0; i <= 12; i++) {
+            let data = {
+                price: i * 100,
+                title: "product " + i,
+                id: i,
+                inCart: false,
+            };
+            let product = new Product(data);
+            products.push(product);
+            console.log(product.getInfo());
+        }
+        let cart = [];
     </script>
 
--- content/themes/grand-mebel/index.php
+++ content/themes/grand-mebel/index.php
@@ -5,41 +5,71 @@
  * @subpackage your-grand-mebel-template-3
  */
 get_header(); ?>
-    <section class="post__section">
-        <div class="container">
-            <div class="row">
-
-                <?php $content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra nisi id justo consectetur tincidunt. Nulla lobortis diam id rhoncus dapibus. Etiam ipsum lacus, vestibulum vitae luctus laoreet, congue eu orci. Suspendisse potenti. Sed et nunc quis nisi lacinia ultrices malesuada a ante. Donec dictum augue erat, id tincidunt ante condimentum a. Aenean rhoncus fringilla sapien. Nam diam elit, tincidunt eu varius nec, semper vel orci. Vivamus eleifend sapien nec vestibulum sollicitudin. Donec porta leo ac dictum cursus. Aenean gravida ultricies lacinia." ?>
-                <?php for ($i = 1; $i <= 24; $i++) : ?>
-                    <div class='post__wrapper'>
-                        <div class='post'>
-                            <h3>Test Post <?= $i ?></h3>
-                            <article><?= $content ?></article>
-                        </div>
+    <section class="content">
+        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="https://cdn.wallscloud.net/converted/2254642368-Priroda-EGDg-1920x1080-MM-100.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Природа 1</h5>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     </div>
-                <?php endfor; ?>
-
+                </div>
+                <div class="carousel-item">
+                    <img src="https://img3.akspic.ru/image/102919-otrazhenie-dikaya_mestnost-ozero-derevo-priroda-1920x1080.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Природа 2</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="https://s1.1zoom.ru/b5050/975/359653-admin_1920x1080.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Природа 3</h5>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </div>
+                </div>
             </div>
+            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
-    </section>
-
-    <section>
         <div class="container">
             <div class="row">
-                <div class="<?php content_class_by_sidebar(); ?>">
-                    <h1><?php
-                        if (is_day()) : printf('Daily Archives: %s', get_the_date());
-                        elseif (is_month()) : printf('Monthly Archives: %s', get_the_date('F Y'));
-                        elseif (is_year()) : printf('Yearly Archives: %s', get_the_date('Y'));
-                        else : 'Archives';
-                        endif; ?></h1>
-                    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                        <?php get_template_part('loop'); ?>
-                    <?php endwhile;
-                    else: echo '<p>Нет записей.</p>'; endif; ?>
-                    <?php pagination(); ?>
+                <div class="col-sm-4">
+                    <div class="product-category">
+                        <a href="https://shaparbrand.ru/catalog/kardigany-zhenskie-vyazanye-ruchnoj-raboty/">
+                            <h4 class="loop-title"><span>Категория 1</span></h4>
+                            <img src="https://i.pinimg.com/474x/8d/60/0d/8d600dbf11f475b7d59156b7470a85aa.jpg" title="Кардиганы" alt>
+                        </a>
+                    </div>
                 </div>
-                <?php get_sidebar(); ?>
+                <div class="col-sm-4">
+                    <div class="loop loop-product-category loop-grid">
+                        <a href="https://shaparbrand.ru/catalog/svitery-zhenskie-vyazanye-ruchnoj-raboty-dzhempera-pulovery/">
+                            <h4 class="loop-title"><span>Категория 2</span></h4>
+                            <img src="https://i.pinimg.com/474x/4b/02/46/4b0246b399433877779f30eaafa0900b.jpg" title="Свитера" alt>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="loop loop-product-category loop-grid">
+                        <a href="https://shaparbrand.ru/catalog/svitery-zhenskie-vyazanye-ruchnoj-raboty-dzhempera-pulovery/">
+                            <h4 class="loop-title"><span>Категория 3</span></h4>
+                            <img src="https://i.pinimg.com/474x/e5/4a/51/e54a51ef8d19b1bd679b9e3fedf68c9c.jpg" title="Свитера" alt>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
