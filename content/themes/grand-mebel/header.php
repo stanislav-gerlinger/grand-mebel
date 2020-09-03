c5ce32a1434c0d97d7fd5b5457ff569d1515f6bf
volodya <vova.emiryan@yandex.ru>
2020-09-03 18:14:01 +0400

    03.09

--- content/themes/grand-mebel/header.php
+++ content/themes/grand-mebel/header.php
@@ -8,67 +8,32 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<?php /* RSS и всякое */ ?>
-	<link rel="alternate" type="application/rdf+xml" title="RDF mapping" href="<?php bloginfo('rdf_url'); ?>">
-	<link rel="alternate" type="application/rss+xml" title="RSS" href="<?php bloginfo('rss_url'); ?>">
-	<link rel="alternate" type="application/rss+xml" title="Comments RSS" href="<?php bloginfo('comments_rss2_url'); ?>">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-
+    
     <link
         rel="stylesheet"
         href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
         crossorigin="anonymous">
-    <script
-        src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
-    </script>
+    <link rel="stylesheet" type="text/css" href="<?=get_template_directory_uri()?>/style.css">
+
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script
         src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
         crossorigin="anonymous">
-
     </script>
-	<!--[if lt IE 9]>
-	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
-	<?php wp_head(); ?>
+
+    <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-	<header>
-		<div class="container">
-			<div class="row">
-				<div class="col-sm-6 col-md-4 col-lg-3" style="background-color: #00aadc;">
-					kupibutton
-                    <button class="btn btn-secondary">...</button>
-				</div>
-                <div class="col-sm-6 col-md-4 col-lg-3" style="background-color: #00aadc;">
-					kupibaton
-				</div>
-                <div class="col-sm-6 col-md-4 col-lg-3" style="background-color: #00aadc;">
-					kupibaton
-				</div>
-			</div>
-		</div>
-        <div class="progress">
-            <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
-        </div>
-	</header>
-
-    <main>
-    <script>
-        var i = 0;
-        var pb = $(".progress-bar");
-        var timerID = setInterval(() => {
-            i++;
-            if(i>=100) {
-                clearInterval(timerID);
-            }
-            pb.css({width:i+"%"})
-            console.log(i);
-        }, 100)
-
-    </script>
-
+  <header>
+    <div class="container">
+    	<a href="http://grand-mebel.loc/products/">Товары</a>    
+    </div>
+  </header>
