def9d21191af8f9da68f16bda036d9c5309ab058
vova.emiryan <vova.emiryan@yandex.ru>
2020-10-03 01:42:00 +0400

    about-us fix

--- content/themes/grand-mebel/page-about_us.php
+++ content/themes/grand-mebel/page-about_us.php
@@ -6,31 +6,51 @@
  * Template Name: Страница с шаблоном "О нас"
  */
 get_header(); ?>
-    <div class="content">
+    <div class="container">
         <p><b>Мы компания Гранд мебель, иы продаем дерево, железо, двп, дсп и все что вы захотите!</b></p>
-        <form>
+        <section class="form">
+            <form>
+                <fieldset>
+                    <legend>Напишите нам</legend>
+
+                    <div class="row">
+                       <div class="col-sm-3">
+                           <label>Введите ваше имя:</label>
+                       </div>
+                       <div class="col-sm-9">
+                           <input type="text" placeholder="Введите ваше имя">
+                       </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <label>Введите ваш почтовый адрес:</label>
+                        </div>
+                        <div class="col-sm-9">
+                            <input class="email" type="email" placeholder="Введите ваш email">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <label> Напишите отзыв:</label>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <textarea placeholder="Оставьте ваш отзыв о нашем сайте"></textarea>
+                        </div>
+                    </div>
+
+                </fieldset>
+            </form>
+        </section>
+        <section>
             <fieldset>
-                <legend>Напишите нам</legend>
-                <label>Введите ваше имя:
-                <br>
-                <input type="text" placeholder="Введите ваше имя">
-            </label>
-                <br>
-                <label>Введите ваш почтовый адрес:
-                <br>
-                <input class="email" type="email" placeholder="Введите ваш email">
-            </label>
-                <br>
-                <label> Напишите отзыв:
-              <br>
-              <textarea cols="40" rows="10" placeholder="Оставьте ваш отзыв о нашем сайте"></textarea>
-            </label>
+                <legend>Наше местоположение</legend>
+                <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A3a71753da21b675bd82b35ef05787eac56a54969a78ff2fe92388fd119eadfe7&amp;width=676&amp;height=450&amp;lang=ru_RU&amp;scroll=true"></script>
             </fieldset>
-        </form>
-        <br>
-        <fieldset>
-            <legend>Наше местоположение</legend>
-            <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A3a71753da21b675bd82b35ef05787eac56a54969a78ff2fe92388fd119eadfe7&amp;width=676&amp;height=450&amp;lang=ru_RU&amp;scroll=true"></script>
-        </fieldset>
+        </section>
     </div>
 <?php get_footer(); ?>
