62f6d5136484dbd1b280dda44b6ead7b05a644a6
vova.emiryan <vova.emiryan@yandex.ru>
2020-11-25 15:20:22 +0400

    flex fix

--- content/themes/grand-mebel/header.php
+++ content/themes/grand-mebel/header.php
@@ -63,27 +63,25 @@
                         </a>
                     </div>
                 </div>
-                <div class="col-sm-3">
+                <div class="col-sm-2">
                     <div class="media">
-                        <a href="https://vk.com/">
-                            <img src="https://sun9-15.userapi.com/impf/QvepP2_xJrZJqTfcGg1RFK0_66xGldryaJY-xA/RlO-WlUVteo.jpg?size=70x70&quality=90&proxy=1&sign=31316c3e73372553ae27f533920799af">
+                        <a href="https://vk.com/" class="media__circle-href">
+                            <img src="content/themes/grand-mebel/assets/images/vk_icon.svg">
                         </a>
-                        <a href="https://instagram.com/">
-                            <img src="https://sun9-2.userapi.com/impf/oP1kyeaFpgR8MJRAee_xlWNeN7IHv-ThgJcn5A/XNYw_eegDPY.jpg?size=71x70&quality=90&proxy=1&sign=b1b8b8025aaa1735dbabf8bbd7da8765">
+                        <a href="https://instagram.com/" class="media__circle-href">
+                            <img src="content/themes/grand-mebel/assets/images/vk_icon.svg">
                         </a>
-                        <a href="https://facebook.com/">
-                            <img src="https://sun9-15.userapi.com/impf/QvepP2_xJrZJqTfcGg1RFK0_66xGldryaJY-xA/RlO-WlUVteo.jpg?size=70x70&quality=90&proxy=1&sign=31316c3e73372553ae27f533920799af">
+                        <a href="https://facebook.com/" class="media__circle-href">
+                            <img src="content/themes/grand-mebel/assets/images/facebook_icon.svg">
                         </a>
                     </div>
                 </div>
-                <div class="col-sm-2">
+                <div class="col-sm-3">
                     <div class="feedback">
                         <a href="call">
                             <img src="https://sun9-57.userapi.com/impf/VJi_j2S2C4hUtD5GQPDcB53tRjd4pHaRThgqKA/z8HJqsA7-CU.jpg?size=30x30&quality=90&proxy=1&sign=d8d9c0dcfff10b5d5972422ad52f29bf">
                         </a>
-                        <h5>
-                            <span>+7 (8422) 33-33-33</span>
-                        </h5>
+                        <span>+7 (8422) 33-33-33</span>
                     </div>
                 </div>
             </div>
