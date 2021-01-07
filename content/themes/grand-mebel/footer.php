a8babc58a6eb4346e479bc1b758d51bd8d264521
2oublePeace <vova.emiryan@yandex.ru>
2021-01-07 16:28:24 +0400

    centration

--- content/themes/grand-mebel/footer.php
+++ content/themes/grand-mebel/footer.php
@@ -9,7 +9,7 @@
     <footer>
 		<div class="container">
 			<div class="row">
-				<div class="col-sm-3">
+				<div class="col-sm-2">
                     <div class="logo">
                         <a href="http://grandmebel.loc/">
                             <img title="Логотип"
@@ -24,11 +24,13 @@
                         </a>
                     </div>
                     <div class="adress">
-                        г. Ульяновск, ул. Ленина, д.7, строение 1
+                        <span>г. Ульяновск, ул. Ленина,</span>
+                        <span>дом 7, строение 1</span>
                     </div>
                 </div>
+                <div class="col-sm-1"></div>
                 <div class="col-sm-2">
-                    <h5>Меню</h5>
+                    <h5 class="footer-main-menu__head">Меню</h5>
                     <div class="footer-main-menu">
                         <nav class="navbar navbar-expand-md navbar-light">
                             <?php
@@ -47,7 +49,7 @@
                     </div>
                 </div>
                 <div class="col-sm-2">
-                    <h5>Товары</h5>
+                    <h5 class="footer-products-menu__head">Товары</h5>
                     <div class="footer-products-menu">
                         <nav class="navbar navbar-expand-md navbar-light">
                             <?php
@@ -65,18 +67,22 @@
                         </nav>
                     </div>
                 </div>
-                <div class="col-sm-2">
+                <div class="col-sm-3">
 
                 </div>
-                <div class="col-sm-3">
-                    <div class="feedback">
-                        <a href="call">
-                            <img src="https://sun9-57.userapi.com/impf/VJi_j2S2C4hUtD5GQPDcB53tRjd4pHaRThgqKA/z8HJqsA7-CU.jpg?size=30x30&quality=90&proxy=1&sign=d8d9c0dcfff10b5d5972422ad52f29bf">
-                        </a>
-                        <h5>
-                            <span>+7 (8422) 33-33-33</span>
-                        </h5>
-                    </div>
+                <div class="col-sm-2">
+                    <div class="footer-feedback">
+												<div class="icon">
+													<a href="call">
+															<img src="https://sun9-57.userapi.com/impf/VJi_j2S2C4hUtD5GQPDcB53tRjd4pHaRThgqKA/z8HJqsA7-CU.jpg?size=30x30&quality=90&proxy=1&sign=d8d9c0dcfff10b5d5972422ad52f29bf">
+													</a>
+												</div>
+												<div class="number">
+													<h5>
+															<span>+7 (8422) 33-33-33</span>
+													</h5>
+												</div>
+											</div>
                     <hr>
                     <div class="media">
                         <a href="https://vk.com/">
