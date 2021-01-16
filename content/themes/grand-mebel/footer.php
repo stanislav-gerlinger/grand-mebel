38e1a97ac22c3551558cb53ae23e68e193ce2c36
2oublePeace <vova.emiryan@yandex.ru>
2021-01-16 18:36:06 +0400

    adaptive

--- content/themes/grand-mebel/footer.php
+++ content/themes/grand-mebel/footer.php
@@ -9,7 +9,7 @@
     <footer>
 		<div class="container">
 			<div class="row">
-				<div class="col-sm-2">
+				<div class="col-sm-4 col-md-2" style="margin-bottom: 20px">
 										<div class="logo">
 												<div class="logo__image">
 														<a href="<?= home_url() ?>" class="">
@@ -32,8 +32,8 @@
                         <span>дом 7, строение 1</span>
                     </div>
                 </div>
-                <div class="col-sm-1"></div>
-                <div class="col-sm-2">
+                <div class="col-sm-2 col-md-1"></div>
+                <div class="col-sm-2 d-none d-md-block">
                     <h5 class="footer-main-menu__head">Меню</h5>
                     <div class="footer-main-menu">
                         <nav class="navbar navbar-expand-md navbar-light">
@@ -52,7 +52,7 @@
                         </nav>
                     </div>
                 </div>
-                <div class="col-sm-2">
+                <div class="col-sm-2 d-none d-md-block">
                     <h5 class="footer-products-menu__head">Товары</h5>
                     <div class="footer-products-menu">
                         <nav class="navbar navbar-expand-md navbar-light">
@@ -71,33 +71,35 @@
                         </nav>
                     </div>
                 </div>
-                <div class="col-sm-3">
+                <div class="col-sm-1 col-lg-2 d-none d-md-block">
 
                 </div>
-                <div class="col-sm-2">
-                    <div class="footer-feedback">
-												<div class="icon">
-													<a href="call">
-															<img src="https://sun9-57.userapi.com/impf/VJi_j2S2C4hUtD5GQPDcB53tRjd4pHaRThgqKA/z8HJqsA7-CU.jpg?size=30x30&quality=90&proxy=1&sign=d8d9c0dcfff10b5d5972422ad52f29bf">
-													</a>
+                <div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                    <div class="footer-contacts">
+											<div class="footer-feedback">
+													<div class="icon">
+														<a href="call">
+																<img src="https://sun9-57.userapi.com/impf/VJi_j2S2C4hUtD5GQPDcB53tRjd4pHaRThgqKA/z8HJqsA7-CU.jpg?size=30x30&quality=90&proxy=1&sign=d8d9c0dcfff10b5d5972422ad52f29bf">
+														</a>
+													</div>
+													<div class="number">
+														<h5>
+																<span>+7 (8422) 33-33-33</span>
+														</h5>
+													</div>
 												</div>
-												<div class="number">
-													<h5>
-															<span>+7 (8422) 33-33-33</span>
-													</h5>
-												</div>
-											</div>
-                    <hr>
-										<div class="footer-media">
-                        <a href="https://vk.com/" class="media__circle-href">
-                            <img src="<?= get_template_directory_uri() ?>/assets/images/vk_icon.svg">
-                        </a>
-                        <a href="https://instagram.com/" class="media__circle-href">
-                            <img src="<?= get_template_directory_uri() ?>/assets/images/instagram_icon.svg">
-                        </a>
-                        <a href="https://facebook.com/" class="media__circle-href">
-                            <img src="<?= get_template_directory_uri() ?>/assets/images/facebook_icon.svg">
-                        </a>
+	                    <hr>
+											<div class="footer-media">
+	                        <a href="https://vk.com/" class="media__circle-href">
+	                            <img src="<?= get_template_directory_uri() ?>/assets/images/vk_icon.svg">
+	                        </a>
+	                        <a href="https://instagram.com/" class="media__circle-href">
+	                            <img src="<?= get_template_directory_uri() ?>/assets/images/instagram_icon.svg">
+	                        </a>
+	                        <a href="https://facebook.com/" class="media__circle-href">
+	                            <img src="<?= get_template_directory_uri() ?>/assets/images/facebook_icon.svg">
+	                        </a>
+	                    </div>
                     </div>
                 </div>
 			</div>
--- content/themes/grand-mebel/index.php
+++ content/themes/grand-mebel/index.php
@@ -15,30 +15,28 @@ get_header(); ?>
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="https://sun9-18.userapi.com/impf/m5t13UIQbAgmYGCzIFkgL2tZw2nqbWZ3ZAOKrg/49XRZqqST28.jpg?size=1280x533&quality=90&sign=71307c43490803f12b5270389c9041b8" class="d-block w-100" alt="...">
+                    <img src="./assets/images/slider-img-3.jpg" class="d-block w-100" alt="...">
                     <div class="carousel-caption d-none d-md-block">
                         <h5>Природа 1</h5>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <img src="https://img3.akspic.ru/image/102919-otrazhenie-dikaya_mestnost-ozero-derevo-priroda-1920x1080.jpg" class="d-block w-100" alt="...">
+                    <img src="./assets/images/slider-img-1.jpg" class="d-block w-100" alt="...">
                     <div class="carousel-caption d-none d-md-block">
                         <h5>Природа 2</h5>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <img src="https://s1.1zoom.ru/b5050/975/359653-admin_1920x1080.jpg" class="d-block w-100" alt="...">
+                    <img src="./assets/images/slider-img-2.jpg" class="d-block w-100" alt="...">
                     <div class="carousel-caption d-none d-md-block">
                         <h5>Природа 3</h5>
                         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                     </div>
                 </div>
                 <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true">
-                      <span class="ftp"></span>
-                    </span>
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="sr-only">Previous</span>
                 </a>
                 <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
@@ -48,7 +46,7 @@ get_header(); ?>
         </div>
     </section>
 
-    <section class="categories" style="padding-top: 60px; padding-bottom: 60px;">
+    <section class="categories" style="padding-top: 60px; padding-bottom: 50px;">
         <div class="container">
             <div class="row">
                 <?php // получаем все термины из таксономии product_category
@@ -61,7 +59,7 @@ get_header(); ?>
                     $i=0;
                     foreach ($terms as $term) {
                         ?>
-                        <div class="col-sm-2">
+                        <div class="col-sm-4 col-md-2">
                             <div class="categories-item">
                                 <div class="categories-item__image">
                                     <img src="https://prestizh73.ru/image/cache/catalog/2019/09/162-2-400x400-370x370.jpg" alt="' . sprintf(__('%s', 'my_localization_domain'), $term->name) . '" title="' . sprintf(__('%s', 'my_localization_domain'), $term->name) . '">
